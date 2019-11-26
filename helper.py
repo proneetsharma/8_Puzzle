dad491cf01f6b024acfaf928ec39c0738c657e4c
proneetsharma <sproneet@gmail.com>
2019-11-26 22:45:28 +0100

    create a Puzzle class and calculate manhattan

--- helper.py
+++ helper.py
@@ -54,3 +54,40 @@ class state():
             copy_state[x][y], copy_state[m][n] = copy_state[m][n], copy_state[x][y]
             child.append(state(self.f, self.g+1, self, copy_state))
         return child
+
+
+class Puzzle():
+
+    def __init__(self, start_data, goal_data):
+        self.open = list()
+        self.priority_queue = []
+        self.start = start_data
+        self.goal = goal_data
+        self.initial_state = self.convert_2D(start_data)
+        self.goal_data_list = [i for i in range(0, PUZZLE_TYPE + 1)]
+        self.goal_state = self.convert_2D(self.goal_data_list)
+        self.path = [] 
+        self.execution = 0
+        self.count = 0
+
+    # Convert list of initial state to 2D
+    def convert_2D(self, puzzle):
+        puzzle_2D = []
+        row = []
+        for idx, val in enumerate(puzzle):
+            row.append(val)
+            if (idx + 1) % ROW_SIZE == 0:     
+                puzzle_2D.append(row)
+                row = []
+        return state(0, 0, None, puzzle_2D)
+
+    # Calculate manhattan as heuristic
+    def manhattan(self, input_data):
+        distance = 0
+        for i in range(ROW_SIZE):
+            for j in range(ROW_SIZE):
+                if input_data[i][j] != self.goal_state.data[i][j] and input_data[i][j] != 0:
+                    A = self.goal_data_list.index(input_data[i][j])
+                    x, y = divmod(A, ROW_SIZE)
+                    distance += abs(x - i) + abs(y - j)
+        return distance
