46244b6ee2b89dcea10c8804e3cf5a34a2f69c32
proneetsharma <sproneet@gmail.com>
2019-11-28 14:46:15 +0100

    add main function

--- A_star_search.py
+++ A_star_search.py
@@ -5,3 +5,27 @@ import sys
 import timeit
 from helper import *
 
+
+
+
+if __name__ == '__main__':       
+
+    goal_state_list = [i for i in range(0, PUZZLE_TYPE + 1)]
+
+    opt = int(sys.argv[1])
+    if opt == 1 or opt == 2:
+
+        if opt ==1:
+            print("\nRunning A star search with Manhattan Dist heuristic\n")
+            file_name = "A_star_manhattan"
+
+        else:
+            print("\nRunning A star search with Misplaced Tiles heuristic\n")
+            file_name = "A_star_misplaced_tiles"
+
+
+        puzzle_8 = [0, 1, 2, 3, 4, 5, 8, 6, 7] # Initial Configuration for testing
+        print("Initial Configuration")
+        board = Puzzle(puzzle_8, goal_state_list)
+        Astar_search(board)
+        board.write_to_file(file_name+"1")
